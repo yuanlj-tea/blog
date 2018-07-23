a59e3d800d0be465b9ac5db9bd2f0cee3ade40d3
Allen <1107806823@qq.com>
2018-07-23 16:19:36 +0800

    jwt

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -11,31 +11,67 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ApiController extends Controller
 {
-    /*注册*/
-    public function register(Request $request)
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
     {
-        $input = $request->all();
-        $input['password'] = Hash::make($input['password']);
-        //User::create($input);
-        return response()->json(['result'=>true]);
-    }
+        $credentials = request(['email', 'password']);
 
-    /*登陆*/
-    public function login(Request $request)
-    {
-        $input = $request->all();
-
-        if (!$token = JWTAuth::attempt($input)) {
-            return response()->json(['result' => '邮箱或密码错误.']);
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
-        return response()->json(['result' => $token]);
+
+        return $this->respondWithToken($token);
     }
 
-    /*获取用户信息*/
-    public function get_user_details(Request $request)
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
     {
-        $input = $request->all();
-        $user = JWTAuth::toUser($input['token']);
-        return response()->json(['result' => $user]);
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
 }
