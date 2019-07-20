e3cac377043922ad21bb1203b7f4af1d0c159619
Allen <1107806823@qq.com>
2019-07-20 15:58:55 +0800

    remove useless code

--- app/Console/Commands/Inspire.php
+++ app/Console/Commands/Inspire.php
@@ -31,8 +31,6 @@ class Inspire extends Command
      */
     public function handle()
     {
-        pp("\033[32mHello, world!","\033[32mHello, world!");
-        die;
         $config = \Kafka\ConsumerConfig::getInstance();
         $config->setMetadataRefreshIntervalMs(10000);
         $config->setMetadataBrokerList('127.0.0.1:9092');
