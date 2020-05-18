b3d56d66b9b5399f28700dc84872fa5fdf4f4257
苑龙杰 <yuanlj@kingnet.com>
2020-05-18 10:03:31 +0800

    基于redis zset实现延迟队列

--- app/Console/Commands/DelayQueue.php
+++ app/Console/Commands/DelayQueue.php
@@ -43,22 +43,33 @@ class DelayQueue extends Command
     local min          = KEYS[2]
     local max          = KEYS[3]
 
-    local ok = redis.call('ZRANGEBYSCORE', delayQueue, min,max,'limit',min,max)
+    local ok = redis.call('ZRANGEBYSCORE', delayQueue, min, max, 'limit', min, max)
     
     -- lua table的key是从1开始的
     if (ok[1]) then
-        redis.call('ZREM',delayQueue,ok[1])
+        redis.call('ZREM', delayQueue, ok[1])
     end
     
     return ok
 LUA;
-        $redis = RedisPHP::connection();
 
+        $redis = RedisPHP::connection();
         $delayQueue = 'test_delay_queue';
         $min = 0;
-        $max = time();
-        $res = $redis->eval($script, 3, $delayQueue, $min, $max);
-        pd($res);
+
+        while (true) {
+            try {
+                $max = time();
+                $data = $redis->eval($script, 3, $delayQueue, $min, $max);
+                if (!empty($data)) {
+                    pp(sprintf("[%s] %s", date('Y-m-d H:i:s'), print_r($data, true)));
+                }
+            } catch (\Exception $e) {
+                $errMsg = sprintf("[time] %s [file] %s [line] %s [msg] %s", date('Y-m-d H:i:s'), $e->getFile(), $e->getLine(), $e->getMessage());
+                $this->error($errMsg);
+            }
+        }
+
 
     }
 }
